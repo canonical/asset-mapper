24f652d895bcc611b2e961e686fc135284c292a4
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-01-13 11:00:23 +0000

    Format code with black

---
+++ README.md
@@ -0,0 +1,3 @@
+# UbuntuDesign asset mapper
+
+A mapping class for manipulating assets in the ubuntu assets server.
--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ubuntudesign-asset-mapper',
-    version='0.10',
-    author='Robin',
-    author_email='robin.winslow@canonical.com',
-    url='https://github.com/ubuntudesign/asset-mapper',
-    packages=[
-        'ubuntudesign'
-    ],
-    description=(
-        'A mapping class for using the Ubuntu asset server.'
-    ),
-    long_description=open('README.md').read(),
+    name="ubuntudesign-asset-mapper",
+    version="0.10",
+    author="Robin",
+    author_email="robin.winslow@canonical.com",
+    url="https://github.com/ubuntudesign/asset-mapper",
+    packages=["ubuntudesign"],
+    description=("A mapping class for using the Ubuntu asset server."),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=[
-        "requests >= 2.0"
-    ]
+    install_requires=["requests >= 2.0"],
 )
